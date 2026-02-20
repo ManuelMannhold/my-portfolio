f839490e6bbfbcc1c0ff09261e0a6c29f02bd3e9
ManuelMannhold <M.Mannhold@gmx.de>
2026-02-20 11:38:31 +0100

    create project

---
+++ src/app/core/components/custom-cursor/custom-cursor/custom-cursor.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-custom-cursor',
+  imports: [],
+  templateUrl: './custom-cursor.html',
+  styleUrl: './custom-cursor.scss',
+})
+export class CustomCursor {
+
+}
