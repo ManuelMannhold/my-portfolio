1872a0e0a3542f84fc6a702380a88bc46ee0a3c3
ManuelMannhold <M.Mannhold@gmx.de>
2026-02-21 22:14:09 +0100

    add tech-stack

--- src/app/core/features/tech-stack/tech-stack/tech-stack.ts
+++ src/app/core/features/tech-stack/tech-stack/tech-stack.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { MatIcon, MatIconModule } from '@angular/material/icon';
+import { AppComponent } from '../../../../app.component';
 
 @Component({
   selector: 'app-tech-stack',
-  imports: [],
+  imports: [MatIcon, MatIconModule],
   templateUrl: './tech-stack.html',
   styleUrl: './tech-stack.scss',
 })
 export class TechStack {
+  private appComponent = inject(AppComponent);
 
+  get expandedTile() {
+    return this.appComponent.expandedTile;
+  }
 }
