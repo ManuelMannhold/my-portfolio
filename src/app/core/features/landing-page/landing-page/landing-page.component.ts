02b0813cdd3a81925b7832f057d6372e12cfdfb2
Manuel Mannhold <m.mannhold@gmx.de>
2026-04-21 20:39:25 +0200

    add job-title

--- src/app/core/features/landing-page/landing-page/landing-page.component.ts
+++ src/app/core/features/landing-page/landing-page/landing-page.component.ts
@@ -1,13 +1,17 @@
 import { Component, inject } from '@angular/core';
 import { TranslateService, TranslateModule } from '@ngx-translate/core';
+import { ModeService } from '../../../services/mode.service';
+import { CommonModule, AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-landing-page',
-  imports: [TranslateModule],
+  imports: [TranslateModule, CommonModule, AsyncPipe],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.scss'
 })
 export class LandingPageComponent {
+  protected modeService = inject(ModeService);
+  isAdminMode$ = this.modeService.mode$;
   private translate = inject(TranslateService);
 
   scrollToContact() {
